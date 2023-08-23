28b46f4b4ae4aa8d6dfe9e2e57201db7eaa2c396
Dusty Shaw <shustydaw@gmail.com>
2023-08-22 20:59:20 -0600

    Add project files.

---
+++ practice-chapt1/Program.cs
@@ -0,0 +1,67 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/error");
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 10).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+var feelings = new[]
+{
+    "sad", "happy", "excited", "hopeful", "lucky", "loving", "amused", "satisfied", "proud", "optimistic", "Admiration"
+};
+
+app.MapGet("/moodforcast", (int index) => "I am feeling " + feelings[index]);
+
+app.MapGet("/moodforcastwithobject", () =>
+{
+    return new MoodObject();
+});
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+internal record MoodObject()
+{
+    public int Mood = Random.Shared.Next(0, 10);
+}
+
